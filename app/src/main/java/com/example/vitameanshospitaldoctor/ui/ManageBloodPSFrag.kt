194a845c95cfa9e7650f7c070da035c042db3b8e
hamin <khm0651@naver.com>
2021-06-21 13:17:00 +0900

    first commit

---
+++ app/src/main/java/com/example/vitameanshospitaldoctor/ui/ManageBloodPSFrag.kt
@@ -0,0 +1,24 @@
+package com.example.vitameanshospitaldoctor.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.vitameanshospitaldoctor.R
+
+
+class ManageBloodPSFrag : Fragment() {
+
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_manage_blood_p_s, container, false)
+    }
+
+
+}
